d0f0ec510dbcc2b718a2c699b7158ee24e7c4780
Zoheb Shaikh <zoheb.shaikh@diamond.ac.uk>
2024-05-10 17:46:35 +0100

    added extra_dso_load

--- python/pvxslibs/ioc.py
+++ python/pvxslibs/ioc.py
@@ -5,9 +5,9 @@ from epicscorelibs import ioc
 from setuptools_dso.runtime import find_dso
 import pvxslibs.path
 
-pvxsIoc = ctypes.CDLL(find_dso("pvxslibs.lib.pvxsIoc"), ctypes.RTLD_GLOBAL)
 os.environ.setdefault("PVXS_QSRV_ENABLE", "YES")
 
 if __name__ == "__main__":
     extra_dbd_load = [(b"pvxsIoc.dbd", pvxslibs.path.dbd_path)]
-    ioc.main(extra_dbd_load=extra_dbd_load)
+    extra_dso_load = ["pvxslibs.lib.pvxsIoc"]
+    ioc.main(extra_dbd_load=extra_dbd_load,extra_dso_load=extra_dso_load)
