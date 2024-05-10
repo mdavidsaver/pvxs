47fb4bee15b51673e313640b5eb5e8c17e4f7755
Zoheb Shaikh <zoheb.shaikh@diamond.ac.uk>
2024-05-10 14:35:27 +0100

    changed file name

--- python/pvxslibs/ioc.py
+++ python/pvxslibs/ioc.py
@@ -5,10 +5,9 @@ from epicscorelibs import ioc
 from setuptools_dso.runtime import find_dso
 import pvxslibs.path
 
-# TODO: Quetion: Will this work if these lines are here or should they be inside epicscorelibs?
 pvxsIoc = ctypes.CDLL(find_dso("pvxslibs.lib.pvxsIoc"), ctypes.RTLD_GLOBAL)
 os.environ.setdefault("PVXS_QSRV_ENABLE", "YES")
 
 if __name__ == "__main__":
-    db_load = dbd_load=[(b'base.dbd',None), (b'qsrv.dbd',None),(b"pvxsIoc.dbd", pvxslibs.path.dbd_path)]
-    ioc.main(db_load=dbd_load)
+    extra_dbd_load = [(b"pvxsIoc.dbd", pvxslibs.path.dbd_path)]
+    ioc.main(extra_dbd_load=extra_dbd_load)
