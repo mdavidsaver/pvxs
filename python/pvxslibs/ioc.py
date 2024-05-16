74416723c75c6783ff08907612fdaa7e767dff95
Zoheb Shaikh <zoheb.shaikh@diamond.ac.uk>
2024-05-16 10:06:32 +0100

    code review changes

--- python/pvxslibs/ioc.py
+++ python/pvxslibs/ioc.py
@@ -3,9 +3,8 @@ import os
 from epicscorelibs import ioc
 import pvxslibs.path
 
-os.environ.setdefault("PVXS_QSRV_ENABLE", "YES")
-
 if __name__ == "__main__":
-    extra_dbd_load = [(b"pvxsIoc.dbd", pvxslibs.path.dbd_path)]
-    extra_dso_load = ["pvxslibs.lib.pvxsIoc"]
+    os.environ.setdefault("PVXS_QSRV_ENABLE", "YES")
+    extra_dbd_load = (("pvxsIoc.dbd", pvxslibs.path.dbd_path),)
+    extra_dso_load = ({"dso":"pvxslibs.lib.pvxsIoc"},)
     ioc.main(extra_dbd_load=extra_dbd_load,extra_dso_load=extra_dso_load)
