f53f2dcae16352def7f665d61e6d1815e88f6871
Zoheb Shaikh <zoheb.shaikh@diamond.ac.uk>
2024-05-17 10:56:51 +0100

    changed dso call pattern

--- python/pvxslibs/ioc.py
+++ python/pvxslibs/ioc.py
@@ -5,6 +5,6 @@ import pvxslibs.path
 
 if __name__ == "__main__":
     os.environ.setdefault("PVXS_QSRV_ENABLE", "YES")
-    pvxs_dbd_load = (("pvxsIoc.dbd", pvxslibs.path.dbd_path),)
-    pvxs_dso_load = ({"dso":"pvxslibs.lib.pvxsIoc"},)
-    ioc.main(extra_dbd_load=pvxs_dbd_load,extra_dso_load=pvxs_dso_load)
+    pvxs_dbd_load = (("pvxsIoc.dbd", pvxslibs.path.dbd_path), )
+    pvxs_dso_load = ("pvxslibs.lib.pvxsIoc", )
+    ioc.main(extra_dbd_load=pvxs_dbd_load, extra_dso_load=pvxs_dso_load)
